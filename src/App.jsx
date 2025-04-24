40bddde3bab24fc371492c76c5c5b4ce990d6ec2
Jesus Hernan <jesushernan3@gmail.com>
2025-04-23 22:25:39 -0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <div>deberia andar</div>
+      <h1 className="bg-red-300">APRENDIENDO</h1>
+    </>
+  );
+}
+
+export default App;
